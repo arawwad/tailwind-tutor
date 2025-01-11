bdc49e77581b2360bd9556044cca45ce68e794bb
Abdallah Awwad <ar.awwad6@gmail.com>
2025-01-11 14:47:19 +0100

    :update readme with website link

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,9 @@ function App() {
           id="target"
           style={{ width: 500, height: 500 }}
         >
-          <h1 className="text-4xl font-bold text-gray-800 my-4">Preview</h1>
+          <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">
+            Click Me!
+          </button>
         </div>
       </div>
       <HtmlEditor code={code} setCode={setCode} />
